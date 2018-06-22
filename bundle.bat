5e6e6ce2ca158e95bebb109243a56fcebfaf6b8e
Mark Wallace <mark.wallace@semanticarts.com>
2018-06-22 10:40:54 -0400

    Added windows script to bundle up release files

---
+++ bundle.bat
@@ -0,0 +1,18 @@
+REM Bundle releasable files into a folder for web download 
+
+setlocal
+set VERSION=8.0.0
+
+cd OntologyFiles
+set ZIPDIR=..\gist%VERSION%_webDownload
+mkdir %ZIPDIR%
+for %%f in (*.owl) do copy %%f %ZIPDIR%\%%~nf%VERSION%%%~xf
+
+set DOCDIR=%ZIPDIR%\Documentation
+mkdir %DOCDIR%
+copy ReleaseNotes_gist%VERSION%.txt %DOCDIR%
+copy gist.vsdx %DOCDIR%\gist%VERSION%.vsdx
+
+set DEPDIR=%DOCDIR%\Deprecated
+mkdir %DEPDIR%
+move %ZIPDIR%\gistDeprecated*.owl %DEPDIR%
