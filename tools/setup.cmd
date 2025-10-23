427767f4af4a648915adae5dd1a0d4ca4f7d11ae
Jamie-SA <jg@semarts>
2025-10-22 20:11:13 -0600

    Rename files

--- tools/setup.cmd
+++ tools/setup.cmd
@@ -19,13 +19,16 @@ set -x
 # Copy pre-commit to the git hooks directory
 cp "${base_dir}/tools/pre-commit" "${base_dir}/.git/hooks/"
 
-# Make pre-commit hook executable. 
+# Make pre-commit hook executable.
 chmod +x "${base_dir}/.git/hooks/pre-commit"
 
 # Ensure that the serializer pre-commit hook is executable.
 chmod +x "${base_dir}/tools/serializer/pre-commit"
 
-# Don't track executable flags on files in this repository (this is not a global setting). 
+# Ensure that the serializer pre-commit hook is executable.
+chmod +x "${base_dir}/tools/check_default_prefix/check_default_prefix.py"
+
+# Don't track executable flags on files in this repository (this is not a global setting).
 git config core.filemode false
 
 # Exit linux shell
