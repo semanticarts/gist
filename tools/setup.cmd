94a84e4ce94c33adbd00f935282a338a6b65e9cf
Rebecca Younes <rebecca.younes@semanticarts.com>
2025-10-26 11:26:04 -0400

    Remove core.filemode false setting

--- tools/setup.cmd
+++ tools/setup.cmd
@@ -31,9 +31,6 @@ chmod +x "${base_dir}/tools/serializer/pre-commit"
 # Ensure that check_default_prefix.py is executable.
 chmod +x "${base_dir}/tools/check_default_prefix/check_default_prefix.py"
 
-# Don't track executable flags on files in this repository (this is not a global setting).
-git config core.filemode false
-
 # Exit linux shell
 exit
 
